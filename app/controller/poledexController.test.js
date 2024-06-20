2246dc95eda17b9589b29a3680837b4235fd6893
Zeke <egonzalez@student.fullsail.edu>
2024-06-20 13:37:26 -0400

    Started Jest Testing

---
+++ app/controller/poledexController.test.js
@@ -0,0 +1,19 @@
+const {
+    getAllPkmn,
+    getPkmnbyID,
+    getPkmnbyName,
+    delPkmn,
+    createPkmn,
+    editPkmn,
+    uploadAll,
+    getPkmnByWk,
+    delAll,
+    filter,
+    select
+} = require("./pokedexController")
+
+describe("Return Pokemon",()=>{
+    test("The Method Return a Pokémon",()=>{
+        const result = 
+    })
+});
