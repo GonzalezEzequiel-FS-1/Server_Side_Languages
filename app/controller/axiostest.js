9cdd6bab28411a37aef9bade5529c3093fb57cb2
Zeke <egonzalez@student.fullsail.edu>
2024-06-22 13:55:37 -0400

    2 tests done, finally!!!!

--- app/controller/axiostest.js
+++ app/controller/axiostest.js
@@ -3,10 +3,9 @@ const axios = require("axios");
 const testing = async () => {
     try {
         const response = await axios.get("http://127.0.0.1:6969/api/pokedex/genone");
-        //const name = response.data.data[0].name;
-        //console.log(name)
-        console.log()
-        return response;
+        const data = response.data.data;
+        //console.log(data)
+        return data;
     } catch (error) {
         console.error("Error fetching data:", error);
         throw error;
--- app/controller/axiostest.test.js
+++ app/controller/axiostest.test.js
@@ -1,11 +1,38 @@
 const testing = require("./axiostest");
-const restest =  testing();
-const name = restest.data.data[0].name;
-console.log(name)
+
+
 
 describe("Testing response's structure", () => {
-    test("Does the response contain a name", async () => {
-            const restest = await testing();
-            console.log("Response:", restest);
+    test("Does the response contain the number", async () => {
+            const response = await testing();
+            const number = response[0].num
+            expect(number).toBe(1);
+            
     });
 });
+
+describe("Testing response's structure", () => {
+    test("Is the height a number?", async () => {
+            const response = await testing();
+            const number = response[0].num
+            expect(typeof number).toBe('number');
+            
+    });
+});
+
+/*
+
+const response = await testing();
+            const number = response[0].num
+            const name = response[0].name
+            const image = response[0].img
+            const type = response[0].type
+            const height = response[0].height
+            const weight = response[0].weight
+            const weaknesses = response[0].weaknesses
+            const nextEvolution = response[0].next_evolution
+            const prevEvolution = response[0].prev_evolution
+            
+            console.log(`Number >>>  ${number}\nName   >>> ${name}\nImage  >>>  ${image}\nType >>> ${type}\nHeight   >>>  ${height}\nWeight >>> ${weight}\nWeaknesses   >>>  ${weaknesses}\nNext Evolution >>> ${nextEvolution}\nPrevious Evolution >>> ${prevEvolution}`);
+
+*/
